2b4785b806719107ad4ad75b64862b9dbc1ef686
tony-bie <A01424547@tec.mx>
2026-03-25 15:37:14 -0600

    Register created

--- project/my-app/src/App.js
+++ project/my-app/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [mensaje, setMensaje] = useState('');
+
+  async function regsiter(){
+    const email = document.getElementById('Email').value;
+    const username = document.getElementById('Username').value;
+    const password = document.getElementById('Password').value;
+
+    const response = await fetch('http://localhost:8080/register',{
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, username, password })
+    })
+    const data = await response.json();
+
+    if (response.ok){
+      setMensaje('Register success');
+    }
+    else{      
+      setMensaje('Register fail');
+    } 
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Register now</h1>
+        <div>
+            <label for="Email">Email</label>
+            <input type ="text" id="Email" name="Email" />
+        </div>
+        <div class>
+            <label for="Name">Username</label>
+            <input type ="text" id="Username" name="Username" />
+        </div>
+        <div>
+            <label for="Password">Password</label>
+            <input type ="text" id="Password" name="Password" />
+        </div>
+        <div>
+            <button onClick={regsiter}>Sign up</button>
+        </div>
+        <p>{mensaje}</p>
     </div>
   );
 }
--- project/node_runtime/index.js
+++ project/node_runtime/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const supabase = require('./supabase')
+const cors = require('cors');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.get('/check-table', async (req, res) => {
   const { data, error } = await supabase
@@ -14,9 +16,32 @@ app.get('/check-table', async (req, res) => {
   res.json({ exists: true, message: "Existe tabla users y id_user" })
 })
 
-app.use((req, res) => {
-    res.redirect('/check-table');;
-});
+app.post('/register', async(req, res) =>{
+  const {email, username, password} = req.body
+
+  const {data, error} = await supabase
+    .from('users')
+    .insert({email: email, username: username, password_hash: password})
+    .select();
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const { error: roleError } = await supabase
+    .from('role')
+    .insert({
+      id_user: data[0].id_user,
+      status: 'viewer'
+    });
+
+  if (roleError) {
+    console.error('Error asignando rol:', roleError);
+  }
+
+  res.status(201).json({ message: 'Usuario creado', user: data[0] }); 
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
