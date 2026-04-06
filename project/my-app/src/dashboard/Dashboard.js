e1ed5353ab4ca04492eab394188b3676b4b65d70
tony-bie <A01424547@tec.mx>
2026-04-05 19:14:35 -0600

    Login with argon2, jwt and an env.example added

--- project/my-app/src/dashboard/Dashboard.js
+++ project/my-app/src/dashboard/Dashboard.js
@@ -1,6 +1,13 @@
-function Dashboard(){
+function Dashboard({ onLogin }){
+    function log_out(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('username')
+
+        onLogin();
+    }
     return (
         <div>
+            <button onClick={log_out}>Login out</button>
             <h1>Login success</h1>
         </div>
     );
--- project/node_runtime/src/auth/auth.routes.js
+++ project/node_runtime/src/auth/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { login, register } = require('./auth.controller');
+const { login, register, verify_token } = require('./auth.controller');
 
 router.post('/login', login);
 router.post('/register', register);
+router.get('/verify', verify_token);
 
 module.exports = router;
